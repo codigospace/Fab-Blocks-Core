364b20b152a68ead61086ad0c0561133908a5ea5
Leonardo Coaquira <coaquiraleonardo19@gmail.com>
2024-06-13 16:46:48 -0500

    avance libreria

--- Modular/setup.py
+++ Modular/setup.py
@@ -12,5 +12,22 @@ setup(
     name='pymodular',
     version='0.1',
     py_modules=['pymodular'],
+)
+
+setup(
+    name='pymodular',
+    version='0.1',
+    packages=find_packages(),
+    description='PyModular',
+    author='Codigo',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
     install_requires=install_requires,
 )
