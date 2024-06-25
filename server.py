26f235e000142331307582ac06202aa2fc65546e
Leonardo Coaquira <coaquiraleonardo19@gmail.com>
2024-06-25 18:18:47 -0500

    change port

--- server.py
+++ server.py
@@ -308,4 +308,4 @@ def serve_documentation(filename):
         return jsonify({"status": "error", "message": "Archivo no encontrado."}), 404
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=80, debug=False)
+    socketio.run(app, host='0.0.0.0', port=5000, debug=False)
