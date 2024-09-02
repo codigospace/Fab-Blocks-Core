cdb39cad7f38acf59a3080d6b0cb1242b50c3f01
Leonardo Coaquira <coaquiraleonardo19@gmail.com>
2024-09-02 17:58:55 -0500

    cambios en setup

--- Modular/setup.py
+++ Modular/setup.py
@@ -5,21 +5,17 @@ install_requires = [
     'adafruit-circuitpython-ads1x15',
 ]
 
+# Añadir RPi.GPIO solo si estamos en una máquina Linux con arquitectura ARM
 if platform.system() == 'Linux' and 'arm' in platform.machine():
     install_requires.append('RPi.GPIO')
 
 setup(
     name='pymodular',
     version='0.1',
-    py_modules=['pymodular2'],
-)
-
-setup(
-    name='pymodular',
-    version='0.1',
-    packages=find_packages(),
     description='PyModular',
     author='Codigo',
+    py_modules=['pymodular'],  # Si pymodular2.py es un módulo independiente
+    packages=find_packages(),  # Encuentra y lista automáticamente todos los paquetes
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
